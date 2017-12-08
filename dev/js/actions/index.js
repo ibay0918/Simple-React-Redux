62555d300e48e7a5889768276552ce40d58b1a32
ibaylis <ibay0918@gmail.com>
2017-12-08 11:07:43 -0500

    Create index.js

---
+++ dev/js/actions/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const selectUser = (user) => {
+  console.log("You clicked on user: ", user.first);
+  return {
+    type: "USER_SELECTED",
+    payload: user
+  }
+};
